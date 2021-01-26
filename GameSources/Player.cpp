980c64fcf43f8b499cd2c1d70091b9285b42270d
Hikaru Nishimaki <zeal20300002@c-league.jp>
2021-01-26 13:44:36 +0900

    プレイヤーをパッドの方向キーに移動できるようにした

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -18,7 +18,27 @@ namespace basecross{
 
 	void Player::OnUpdate()
 	{
+		auto& app = App::GetApp();  // アプリケーションオブジェクトを取得する
 
+		auto device = app->GetInputDevice();
+		const auto& pad = device.GetControlerVec()[0];
+
+		auto transComp = GetComponent<Transform>();
+		auto pos = transComp->GetPosition();
+
+		if (pad.wPressedButtons & XINPUT_GAMEPAD_DPAD_LEFT)
+		{
+			// プレイヤーを左に移動させる
+			pos += Vec3(-1.0f, 0.0f, 0.0f);
+		}
+
+		if (pad.wPressedButtons & XINPUT_GAMEPAD_DPAD_RIGHT)
+		{
+			// プレイヤーを右に移動させる
+			pos += Vec3(1.0f, 0.0f, 0.0f);
+		}
+
+		transComp->SetPosition(pos);
 	}
 }
 //end basecross
