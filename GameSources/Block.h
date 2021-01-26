ac5f750ccaa8158174bbcfef089e1dbaada1be28
Hikaru Nishimaki <zeal20300002@c-league.jp>
2021-01-26 14:05:30 +0900

    Blockクラスを追加

---
+++ GameSources/Block.h
@@ -0,0 +1,16 @@
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	class Block : public GameObject
+	{
+	public:
+		Block(const std::shared_ptr<Stage>& stage)
+			: GameObject(stage)
+		{
+		}
+
+		void OnCreate() override;
+	};
+}
