a30e33e662fc575744de7308bdbe8ba5a94c55ee
Hikaru Nishimaki <zeal20300002@c-league.jp>
2021-01-26 15:25:07 +0900

    StageMapクラスを生成

--- GameSources/StageMap.h
+++ GameSources/StageMap.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "stdafx.h"
+# include "Block.h"
+
+namespace basecross {
+	// ステージの配置を管理するためのクラス
+	class StageMap : public GameObject
+	{
+		std::vector < std::shared_ptr<Block>> blocks;
+
+	public : 
+		StageMap(const std::shared_ptr<Stage>& stage)
+			: GameObject(stage)
+		{
+		}
+	};
+}
